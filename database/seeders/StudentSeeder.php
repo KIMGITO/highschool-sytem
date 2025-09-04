d01a67326b924f4f8948574e50c5bdf87959e36c
KIMGITO <kimanthidennis02@gmail.com>
2025-09-04 10:09:38 +0300

    update:make the student factory include gurdian on pivot table

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Guardian;
 use App\Models\Student;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class StudentSeeder extends Seeder
@@ -13,6 +13,9 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
-        Student::factory()->count(3)->create();
+        Student::factory()->count(3)->create()->each(function ($student){
+            $guardians = Guardian::all();
+            $student->guardians->attach($guardians->random(2)->pluck('id')->toArray());
+        });
     }
 }
